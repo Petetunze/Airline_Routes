4cb0d83916f5f0c4f26aba3f13238a98d4c7f451
Sebb <89808917+Petetunze@users.noreply.github.com>
2022-04-08 20:24:32 -0500

    Update route-records.c
    Comments and fixes

--- route-records.c
+++ route-records.c
@@ -8,31 +8,32 @@ RouteRecord* createRecords(FILE* fileIn){
     char currentLine[50];
     int lineCount = 0;
     int s = 0;
-    int k = 0;
 
     // Get the amount of lines
-    while(fgets(currentLine, 50, fileIn) && k < 1){
+    // s is to skip the header in the CVS file
+    while(fgets(currentLine, 50, fileIn)){
 
         if(s == 1){
             
             lineCount++;
-            k++;
         }
         else{
             s = 1;
         }
     }
-
+    
+    // Set the dynamically allocated array
     RouteRecord* returnPtr = (RouteRecord*)malloc(sizeof(RouteRecord) * lineCount);
     RouteRecord* records = returnPtr;
-
+    
+    // Set every element in each struct's array to 0
     int counter = 0;
     while(counter < lineCount){
 
         int i;
         for(i = 0; i < 6; i++){
 
-            returnPtr->passengersPerMonth[i] = 2;
+            returnPtr->passengersPerMonth[i] = 0;
             //printf("%d", returnPtr->passengersPerMonth[i]);
         }
 
